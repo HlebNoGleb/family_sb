1899c29ffce27b6b01de95cf3be772250c2de9ef
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-03-27 13:33:25 +0300

    create

---
+++ js/script.js
@@ -0,0 +1,50 @@
+document.body.onload = function(){
+	setTimeout(function(){
+		var preloader = document.getElementById('page-preloader');
+		if(!preloader.classList.contains('done')){
+			preloader.classList.add('done');
+			$('body').css({'overflow-y':'scroll'});
+		}
+	}, 2000);
+}
+$('.carusel').slick({
+	arrows: true,
+	dots: false,
+	dotsClass: "my-dots",
+	nextArrow: '.slick-next',
+	prevArrow: '.slick-prev',
+	// draggable: false,
+	// swipe: false
+});
+
+function Length() {
+	var path0 = document.querySelector('#logo0');
+	var path1 = document.querySelector('#logo1');
+	// var path2 = document.querySelector('#logo2');
+	// var path3 = document.querySelector('#logo3');
+	// var path4 = document.querySelector('#logo4');
+	// var path5 = document.querySelector('#logo5');
+	// var path6 = document.querySelector('#logo6');
+	// var path7 = document.querySelector('#logo7');
+	// var path8 = document.querySelector('#logo8');
+	var len0 = Math.round(path0.getTotalLength());
+	var len1 = Math.round(path1.getTotalLength());
+	// var len2 = Math.round(path2.getTotalLength());
+	// var len3 = Math.round(path3.getTotalLength());
+	// var len4 = Math.round(path4.getTotalLength());
+	// var len5 = Math.round(path5.getTotalLength());
+	// var len6 = Math.round(path6.getTotalLength());
+	// var len7 = Math.round(path7.getTotalLength());
+	// var len8 = Math.round(path8.getTotalLength());
+	alert("__" + len0 + "__ " + len1 );
+};
+
+		$(document).ready(function(){
+			// Length();
+		$(".et-hero-tabs-container").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+		});
+		});
