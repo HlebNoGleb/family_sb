3d1d2e9ca18b7d94549deb84360abfb0848bba00
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-04-08 05:04:38 +0300

    end?

--- js/script.js
+++ js/script.js
@@ -3,27 +3,42 @@ document.body.onload = function(){
 		var preloader = document.getElementById('page-preloader');
 		if(!preloader.classList.contains('done')){
 			preloader.classList.add('done');
-			$('body').css({'overflow-y':'scroll'});
+			// $('body').css({'overflow-y':'scroll'});
 				setTimeout(function(){
 						animate_this_shit();
 							}, 2500);
 		}
 	}, 2500);
 }
+// setTimeout(function(){
+//     var preloader = document.getElementById('pade-preloader');
+//     if(!preloader.classList.contains('done')){
+//       preloader.classList.add('done');
+//     }
+//   }, 3000);
 function animate_this_shit() {
 	$('.company_name').addClass('company_name_animation');
 }
+window.setInterval(function(){
+  var number = 1 + Math.floor(Math.random() * 10);
+  console.log(number);
+        if(number>=5){
+     		 $('.company_name').addClass('company_name_animation');
+ 	  }
+  
+      else{
+     		 $('.company_name').removeClass('company_name_animation');
+  	  }
+}, 500);
+ 
 $('.carusel').slick({
 	arrows: true,
 	dots: false,
 	dotsClass: "my-dots",
 	nextArrow: '.slick-next',
 	prevArrow: '.slick-prev',
-	 autoplay: true,
-  autoplaySpeed: 2000,
 
-	// draggable: false,
-	// swipe: false
+
 });
 
 function Length() {
@@ -57,3 +72,74 @@ function Length() {
 		$('body,html').animate({scrollTop: top}, 1500);
 		});
 		});
+$(document).ready(function(){
+
+
+	/* Toggle Video Modal
+  -----------------------------------------*/
+	function toggle_video_modal() {
+	    
+	    // Click on video thumbnail or link
+	    $(".js-trigger-video-modal").on("click", function(e){
+          
+          // prevent default behavior for a-tags, button tags, etc. 
+	        e.preventDefault();
+        
+          // Grab the video ID from the element clicked
+          var id = $(this).attr('data-youtube-id');
+
+          // Autoplay when the modal appears
+          // Note: this is intetnionally disabled on most mobile devices
+          // If critical on mobile, then some alternate method is needed
+          var autoplay = '?autoplay=1';
+
+          // Don't show the 'Related Videos' view when the video ends
+          var related_no = '&rel=0';
+
+          // String the ID and param variables together
+          var src = 'https://www.youtube.com/embed/'+id+autoplay+related_no;
+          
+          // Pass the YouTube video ID into the iframe template...
+          // Set the source on the iframe to match the video ID
+          $("#youtube").attr('src', src);
+          
+          // Add class to the body to visually reveal the modal
+          $("body").addClass("show-video-modal noscroll");
+	    
+      });
+
+	    // Close and Reset the Video Modal
+      function close_video_modal() {
+        
+        event.preventDefault();
+
+        // re-hide the video modal
+        $("body").removeClass("show-video-modal noscroll");
+
+        // reset the source attribute for the iframe template, kills the video
+        $("#youtube").attr('src', '');
+        
+      }
+      // if the 'close' button/element, or the overlay are clicked 
+	    $('body').on('click', '.close-video-modal, .video-modal .overlay', function(event) {
+	        
+          // call the close and reset function
+          close_video_modal();
+	        
+      });
+      // if the ESC key is tapped
+      $('body').keyup(function(e) {
+          // ESC key maps to keycode `27`
+          if (e.keyCode == 27) { 
+            
+            // call the close and reset function
+            close_video_modal();
+            
+          }
+      });
+	}
+	toggle_video_modal();
+
+
+
+});
